26c0bfb7c684e7a53f57ce0d413d5aed33e2d491
Amril Syaifa Yasin <amrilsyaifa@gmail.com>
2018-11-03 17:04:18 +0700

    React.memo for performance and update react, and dom to 16.6.0

--- src/containers/App.js
+++ src/containers/App.js
@@ -43,6 +43,15 @@ class App extends Component {
       console.log('[UPDATE App.js] Inside componentWillUpdate()',nextProps, nextState )
   }
 
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log('[UPDATE App.js] Inside getDerivedStateFromProps()',nextProps, prevState )
+    return prevState
+  }
+
+  getSnapshotBeforeUpdate(){
+    console.log('[UPDATE App.js] Inside getSnapshotBeforeUpdate()')
+  }
+
   componentDidUpdate() {
       console.log('[UPDATE App.js] Inside componentDidUpdate()')
   }
